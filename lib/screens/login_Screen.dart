2642b8c53ae9d8c8a55973ec96201d4b83033ac1
DEVEEN RATNAYAKE <deveenrathnayake@DEVEENs-MacBook-Air.local>
2020-09-05 12:15:51 +0530

    create login page ui

--- lib/screens/login_Screen.dart
+++ lib/screens/login_Screen.dart
@@ -1,50 +1,91 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _emailFieldController = TextEditingController();
+  final _passwordFieldController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+    final emailField = TextField(
+      controller: _emailFieldController,
+      obscureText: false,
+      style: style,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        hintText: "Email",
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+    final passwordField = TextField(
+      controller: _passwordFieldController,
+      obscureText: true,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Password",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+    final loginButon = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Theme.of(context).primaryColor,
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {
+          print(_emailFieldController);
+          print(_passwordFieldController);
+        },
+        child: Text("Login",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Welcome back'),
         backgroundColor: Theme.of(context).primaryColor,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          SizedBox(
-            height: 150.0,
-          ),
-          RaisedButton(
-            color: Theme.of(context).accentColor,
-            padding: EdgeInsets.all(10.0),
-            onPressed: () {
-              Navigator.pushNamed(context, 'InputCollectionScreen');
-            },
-            child: Text(
-              'SIGN IN',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 30.0,
-              ),
+      body: Center(
+        child: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(36.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+//                SizedBox(
+//                  height: 155.0,
+//                  child: Image.asset(
+//                    "assets/logo.png",
+//                    fit: BoxFit.contain,
+//                  ),
+//                ),
+                SizedBox(height: 45.0),
+                emailField,
+                SizedBox(height: 25.0),
+                passwordField,
+                SizedBox(
+                  height: 35.0,
+                ),
+                loginButon,
+                SizedBox(
+                  height: 15.0,
+                ),
+              ],
             ),
           ),
-          SizedBox(
-            height: 30.0,
-          ),
-          RaisedButton(
-            color: Theme.of(context).accentColor,
-            padding: EdgeInsets.all(10.0),
-            onPressed: () {},
-            child: Text(
-              'SIGN UP',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 30.0,
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
