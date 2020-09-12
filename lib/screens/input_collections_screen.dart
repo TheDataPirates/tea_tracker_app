bbe51ea35581ecfa5b007f11027eb7c255837313
DEVEEN RATNAYAKE <deveenrathnayake@DEVEENs-MacBook-Air.local>
2020-09-12 19:00:27 +0530

    change save button to blue

--- lib/screens/input_collections_screen.dart
+++ lib/screens/input_collections_screen.dart
@@ -55,7 +55,7 @@ class _InputCollectionScreenState extends State<InputCollectionScreen> {
                 style: TextStyle(fontSize: 20),
               ),
               materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-              color: Colors.amber,
+              color: Colors.blue,
             ),
           )
         ],
