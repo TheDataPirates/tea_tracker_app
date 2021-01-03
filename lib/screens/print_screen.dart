7e7b31b08d6d58675a429e79ff2f76ec08e3cee3
sew98 <sewmi98@gmail.com>
2021-01-03 11:25:01 +0530

    ui modifications - print screeen

--- lib/screens/print_screen.dart
+++ lib/screens/print_screen.dart
@@ -69,23 +69,24 @@ class _PrintScreenState extends State<PrintScreen> {
                 ),
               ),
             ),
-            // Container(
-            //   child: Text(
-            //     'DEDUCTIONS',
-            //     style: TextStyle(
-            //       fontSize: 30,
-            //       fontWeight: FontWeight.bold,
-            //       letterSpacing: 15,
-            //     ),
-            //   ),
-            // ),
+            Container(
+              child: Text(
+                'DEDUCTIONS',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 15,
+                  color: Colors.black,
+                ),
+              ),
+            ),
             Padding(
               padding:
                   const EdgeInsets.symmetric(vertical: 20.0, horizontal: 40.0),
               child: Container(
                 height: mediaQuery.height * 0.2,
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Container(
                       width: mediaQuery.width * 0.2,
@@ -104,10 +105,9 @@ class _PrintScreenState extends State<PrintScreen> {
                         ),
                       ),
                     ),
-                    SizedBox(width: 400,),
                     Text(
-                      'Total deductions: ',
-                      style: TextStyle(color: const Color(0xFF1B5E20), fontSize: 35, fontWeight: FontWeight.bold),
+                      'Total deductions',
+                      style: TextStyle(color: const Color(0xFF1B5E20), fontSize: 35, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic),
                     ),
                     Container(
                       width: mediaQuery.width * 0.2,
